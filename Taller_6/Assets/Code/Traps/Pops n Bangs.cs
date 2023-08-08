c1288ca2781cca12855d4fb05a7e160482720b3c
iSekkachi <santiago.banol@upb.edu.co>
2023-08-08 14:17:59 -0500

    Startup Trap scripts

--- Taller_6/Assets/Code/Traps/Pops n Bangs.cs
+++ Taller_6/Assets/Code/Traps/Pops n Bangs.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bribery : MonoBehaviour
+public class PopsnBangs : TrapsFather
 {
     // Start is called before the first frame update
     void Start()
