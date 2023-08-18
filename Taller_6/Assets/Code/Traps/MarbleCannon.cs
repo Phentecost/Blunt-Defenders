945068864a308d441e4b47ff8f9f6394b57d0a5d
iSekkachi <santiago.banol@upb.edu.co>
2023-08-17 20:57:02 -0500

    yes

--- Taller_6/Assets/Code/Traps/MarbleCannon.cs
+++ Taller_6/Assets/Code/Traps/MarbleCannon.cs
@@ -8,7 +8,7 @@ public class MarbleCannon : TrapsFather
     [SerializeField] Marble Projectile;
     GameObject target;
     float CDTime = 4f;
-    // Start is called before the first frame update
+
     void Start()
     {
         this.ammo = 10;
