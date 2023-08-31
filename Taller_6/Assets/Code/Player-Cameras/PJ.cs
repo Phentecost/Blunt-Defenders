109ac4f95abfd172d31402a338cb3dc502e13aaf
Pablo <samuelpablo111@gmail.com>
2023-08-31 18:58:15 -0500

    correcion

--- Taller_6/Assets/Code/Player-Cameras/PJ.cs
+++ Taller_6/Assets/Code/Player-Cameras/PJ.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PJ : MonoBehaviour
 {
@@ -9,36 +10,18 @@ public class PJ : MonoBehaviour
     private Vector2 touchOrigin = -Vector2.one;
     private bool canMove = true;
 
+    public PlayerInput playerInput;
+
     void Update()
     {
-        if (Input.touchCount > 0 && canMove)
-        {
-            Touch touch = Input.GetTouch(0);
+        if(Game_Manager._Current_Game_State == Game_Manager.Game_State.Defending) return;
 
-            if (touch.phase == TouchPhase.Began)
-            {
-                touchOrigin = touch.position;
-            }
-            else if (touch.phase == TouchPhase.Moved && touchOrigin != -Vector2.one)
-            {
-                Vector2 touchEnd = touch.position;
-                Vector2 touchDirection = touchEnd - touchOrigin;
-                touchDirection.Normalize();
-
-                Vector2 movement = touchDirection * moveSpeed * Time.deltaTime;
-
-                
-                playerRigidbody.MovePosition(playerRigidbody.position + movement);
-
-                Vector3 cameraPosition = new Vector3(playerRigidbody.position.x, playerRigidbody.position.y, mainCamera.transform.position.z);
-                mainCamera.transform.position = cameraPosition;
-            }
-
-            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
-            {
-                touchOrigin = -Vector2.one;
-            }
-        }
+        Vector2 touchDirection = playerInput.actions["Walk"].ReadValue<Vector2>();
+        touchDirection.Normalize();
+        Vector2 movement = touchDirection * moveSpeed * Time.deltaTime; 
+        playerRigidbody.MovePosition(playerRigidbody.position + movement);
+        Vector3 cameraPosition = new Vector3(playerRigidbody.position.x, playerRigidbody.position.y, mainCamera.transform.position.z);
+        mainCamera.transform.position = cameraPosition;
     }
 
     public void SetCanMove(bool canMove)
