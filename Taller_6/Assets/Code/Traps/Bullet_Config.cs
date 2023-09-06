c3239e7b0d14773f5972798dbd5d964c6915042d
iSekkachi <santiago.banol@upb.edu.co>
2023-09-05 21:25:10 -0500

    yes

--- Taller_6/Assets/Code/Traps/Bullet_Config.cs
+++ Taller_6/Assets/Code/Traps/Bullet_Config.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet_Config : MonoBehaviour
+[CreateAssetMenu]
+public class Bullet_Config : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
